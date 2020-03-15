6f074ff32203f4586f1bf39d6ef81041a7d4abe3
Dmitrsl <daslabuhin@gmail.com>
2020-03-15 22:14:54 +0300

    models

--- train.py
+++ train.py
@@ -1,1 +1,20 @@
-from utils.model import get_model
+'''
+train
+'''
+
+import torch
+from utils.model import get_efficient, get_seresnext
+
+
+def main():
+    '''
+    main
+    '''
+    model = get_efficient('efficientnet-b0', channels='GREY')
+    model_r = get_seresnext("se_resnext50_32x4d", channels='GREY')
+    inp = torch.empty(16, 1, 128, 128)
+    print(model(inp), model_r(inp), inp)
+
+
+if __name__ == '__main__':
+    main()
--- utils/model.py
+++ utils/model.py
@@ -1,7 +1,24 @@
+'''
+get EfficientNet or SE_ResNext
+'''
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import pretrainedmodels
+from efficientnet_pytorch import EfficientNet
+
+
+class Identity(nn.Module):
+    '''
+    inentity
+    '''
+
+    def __init__(self, ):
+        super(Identity, self).__init__()
+
+    def forward(self, input):
+        return input
 
 
 class ResModel(nn.Module):
@@ -11,38 +28,136 @@ class ResModel(nn.Module):
         self.arch = arch
         self.pretrained = pretrained
         self.model = pretrainedmodels.__dict__[self.arch](pretrained=self.pretrained)
-        #self.n_feats = model.last_linear.in_features
-        self.l0 = nn.Linear(2048, 168)
-        self.l1 = nn.Linear(2048, 11)
-        self.l2 = nn.Linear(2048, 7)
+        # self.n_feats = model.last_linear.in_features
+        self.l_0 = nn.Linear(2048, 168)
+        self.l_1 = nn.Linear(2048, 11)
+        self.l_2 = nn.Linear(2048, 7)
 
     def forward(self, x):
-        bs, _, _, _= x.shape
+        bs, _, _, _ = x.shape
         x = self.model.features(x)
         x = F.adaptive_avg_pool2d(x, 1).reshape(bs, -1)
-        l0 = self.l0(x)
-        l1 = self.l1(x)
-        l2 = self.l2(x)
-        return l0, l1, l2
+        l_0 = self.l_0(x)
+        l_1 = self.l_1(x)
+        l_2 = self.l_2(x)
+        return l_0, l_1, l_2
 
-def get_model(arch: str, pretrained: str = "imagenet"):
-    
+
+class Conv2dStaticSamePadding(nn.Conv2d):
+    """ 2D Convolutions like TensorFlow, for a fixed image size"""
+
+    def __init__(self, in_channels, out_channels, kernel_size, image_size=None, **kwargs):
+        super().__init__(in_channels, out_channels, kernel_size, **kwargs)
+        self.stride = self.stride if len(self.stride) == 2 else [self.stride[0]] * 2
+        self.in_channels = 1
+        # Calculate padding based on image size and save it
+        assert image_size is not None
+        ih, iw = image_size if type(image_size) == list else [image_size, image_size]
+        kh, kw = self.weight.size()[-2:]
+        sh, sw = self.stride
+        oh, ow = math.ceil(ih / sh), math.ceil(iw / sw)
+        pad_h = max((oh - 1) * self.stride[0] + (kh - 1) * self.dilation[0] + 1 - ih, 0)
+        pad_w = max((ow - 1) * self.stride[1] + (kw - 1) * self.dilation[1] + 1 - iw, 0)
+        if pad_h > 0 or pad_w > 0:
+            self.static_padding = nn.ZeroPad2d((pad_w // 2, pad_w - pad_w // 2,
+                                                pad_h // 2, pad_h - pad_h // 2))
+        else:
+            self.static_padding = Identity()
+
+    def forward(self, x):
+        x = self.static_padding(x)
+        x = F.conv2d(x, self.weight, self.bias, self.stride, self.padding, self.dilation, self.groups)
+        return x
+
+
+class Efficient(nn.Module):
+    '''
+    Efficient
+    '''
+
+    def __init__(self, arch, pretrained):
+
+        super(Efficient, self).__init__()
+        self.arch = arch
+        if pretrained:
+            self.model = EfficientNet.from_pretrained(self.arch)
+        else:
+            self.model = EfficientNet.from_name(self.arch)
+        # self.n_feats = model.last_linear.in_features
+        self.l_0 = nn.Linear(1000, 168)
+        self.l_1 = nn.Linear(1000, 11)
+        self.l_2 = nn.Linear(1000, 7)
+
+    def forward(self, x):
+        # bs, _, _, _= x.shape
+        x = self.model(x)
+        # x = F.adaptive_avg_pool1d(x, output_size =(bs, 1)) #.reshape(bs, -1)
+        l_0 = self.l_0(x)
+        l_1 = self.l_1(x)
+        l_2 = self.l_2(x)
+        return l_0, l_1, l_2
+
+
+def get_seresnext(arch: str, pretrained: str = "imagenet", channels: str = 'RGB'):
+
+    assert channels in ['RGB', 'GREY']
     model = ResModel(arch, pretrained=pretrained)
-    model_grey = model
 
-    # Sum over the weights to convert the kernel
-    weight_grey = model.model.layer0.conv1.weight.sum(dim=1, keepdim=True)
-    bias = model_grey.model.layer0.conv1.bias
+    if channels == 'RGB':
+        return model
+    elif channels == 'GREY':
+        model_grey = model
+        # Sum over the weights to convert the kernel
+        weight_grey = model.model.layer0.conv1.weight.sum(dim=1, keepdim=True)
+        bias = model_grey.model.layer0.conv1.bias
+        # Instantiate a new convolution module and set weights
+        model_grey.model.layer0.conv1 = torch.nn.Conv2d(1, 64, kernel_size=(7, 7),
+                                                        stride=(2, 2), padding=(3, 3),
+                                                        bias=False)
+        model_grey.model.layer0.conv1.weight = torch.nn.Parameter(weight_grey)
+        model_grey.model.layer0.conv1.bias = bias
+        # model_grey.layer0.relu1 = Mish()
+        # model_grey
+        return model_grey
+    else:
+        print('wrong cannels')
 
-    # Instantiate a new convolution module and set weights
-    model_grey.model.layer0.conv1 = torch.nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
-    model_grey.model.layer0.conv1.weight = torch.nn.Parameter(weight_grey)
-    model_grey.model.layer0.conv1.bias = bias
 
-    #model_grey.layer0.relu1 = Mish()
-    model_grey
+def get_efficient(arch: str, pretrained: str = "imagenet", channels: str = 'RGB'):
 
-    return model_grey
+    assert channels in ['RGB', 'GREY']
+    model = Efficient(arch, pretrained)
 
-model_name = "se_resnext50_32x4d"   
-model = get_model(arch=model_name, pretrained=None) #.eval()
+    if channels == 'RGB':
+        return model
+    elif channels == 'GREY':
+        model_grey = model
+        # Sum over the weights to convert the kernel
+        weight_grey = model.model._conv_stem.weight.sum(dim=1, keepdim=True)
+        bias = model_grey.model._conv_stem.bias
+        # Instantiate a new convolution module and set weights
+        model_grey.model._conv_stem = Conv2dStaticSamePadding(in_channels=1,
+                                                              out_channels=48,
+                                                              image_size=128,
+                                                              kernel_size=(3, 3),
+                                                              stride=(2, 2),
+                                                              bias=False,)
+        # padding=(0, 1, 0, 1))
+        model_grey.model._conv_stem.weight = torch.nn.Parameter(weight_grey)
+        model_grey.model._conv_stem.bias = bias
+
+        return model_grey
+    else:
+        print('wrong cannels')
+
+
+def main():
+
+    model_r = get_seresnext("se_resnext50_32x4d")
+    model_e = get_efficient('efficientnet-b0')
+    input_ = torch.empty(64, 3, 128, 128)
+    print(model_e(input_)[0].shape, model_r(input_)[0].shape)
+
+
+if __name__ == '__main__':
+    main()
